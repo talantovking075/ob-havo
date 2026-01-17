c2948ad5ed694dfc0ec97c07f5304902bc57fc18
talantovking075 <_EMAIL__GITHUB>
2026-01-17 21:00:29 +0600

    ob-havo

---
+++ main.js
@@ -0,0 +1,6 @@
+const api = `https://api.openweathermap.org/data/2.5/weather?q=Tashkent&units=metric&appid=3bcfab247d2495c80244a57d75cfb755`
+
+
+const elsearch = document.querySelector(".input");
+const elclouds = document.querySelector(".card ");
+
